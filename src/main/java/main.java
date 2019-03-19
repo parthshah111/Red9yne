e8bf308326bad95f82fe21c345a6a4bff6a5c61a
daryl101114 <47648433+daryl101114@users.noreply.github.com>
2019-03-19 17:49:06 -0500

    Added Main Class

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
+
+public class main {
+    public static void main(String[] args) throws SQLException {
+
+    }
+}
