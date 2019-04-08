a5437ee38de3cf29399f2c472bbf0fe4f238e201
Daryl <Daryl101114@gmail.com>
2019-04-08 02:11:55 -0500

    GUI popup

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,19 +1,44 @@
 package Main;
 
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
         import org.springframework.boot.CommandLineRunner;
         import org.springframework.boot.SpringApplication;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class Main implements CommandLineRunner {
+public class Main extends Application {
+
+    private ConfigurableApplicationContext springContext;
+    private Parent root;
 
     public static void main(String[] args){
-        SpringApplication.run(Main.class);
+        launch(args);
+    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//    }
+    @Override
+    public void init() throws Exception
+    {
+        springContext = SpringApplication.run(Main.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        root = fxmlLoader.load();
+        super.init();
+        springContext.getAutowireCapableBeanFactory().autowireBean(this);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setScene(new Scene(root,300,300));
+        primaryStage.show();
     }
 
 }
