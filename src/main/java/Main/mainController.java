5f3388017a78d1ccb4894b70ea75eb0a4acddba8
Tan <tvo22@uh.edu>
2019-04-17 09:33:37 -0500

    Add Login Check

--- src/main/java/Main/mainController.java
+++ src/main/java/Main/mainController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,45 +18,35 @@ import java.io.IOException;
 @Component
 public class mainController
 {
+    public TextField userName;
+    public TextField passWord;
     @Autowired
     private ConfigurableApplicationContext springContext;
     @FXML Button Submit;
 
     @FXML
     public void Submit(ActionEvent actionEvent) throws IOException {
+        String username= userName.getText();
+        String password = passWord.getText();
 
-        //################Order scence#####
-//        Stage parent  = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Orders.fxml"));
-//        fxmlLoader.setControllerFactory(springContext::getBean);
-//        Scene scene = new Scene(fxmlLoader.load());
-//        OrdersController jobFormController = fxmlLoader.getController();
-//        jobFormController.setReturnScene(Submit.getScene());
-//        parent.setScene(scene);
 
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Customers.fxml"));
-//        fxmlLoader.setControllerFactory(springContext::getBean);
-//        Parent root = fxmlLoader.load();
-//        Stage stage = new Stage();
-//        stage.setScene(new Scene(root));
-//        stage.show();
+        String realUsername = "Admin";
+        String realPassword = "Admin!";
+            if (realUsername.contentEquals(username) && realPassword.contentEquals(password)) {
+                Stage parent = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MenuOption.fxml"));
+                fxmlLoader.setControllerFactory(springContext::getBean);
+                Scene scene = new Scene(fxmlLoader.load());
+                MenuOptionController menuOptionController = fxmlLoader.getController();
+                menuOptionController.setReturnScene(Submit.getScene());
+                parent.setScene(scene);
+            }
 
-//        Stage parent  = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MenuOption.fxml"));
-//        fxmlLoader.setControllerFactory(springContext::getBean);
-//        Scene scene = new Scene(fxmlLoader.load());
-//        MenuOptionController jobFormController = fxmlLoader.getController();
-//        jobFormController.setReturnScene(Submit.getScene());
-//        parent.setScene(scene);
+            else
+            {
+                System.out.println("Wrong Password");
+            }
 
-        //#####################Order Details###
-        Stage parent  = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MenuOption.fxml"));
-        fxmlLoader.setControllerFactory(springContext::getBean);
-        Scene scene = new Scene(fxmlLoader.load());
-        MenuOptionController menuOptionController = fxmlLoader.getController();
-        menuOptionController.setReturnScene(Submit.getScene());
-        parent.setScene(scene);
     }
 
 }
