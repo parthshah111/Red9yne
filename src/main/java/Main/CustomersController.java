3fc2b0003cc3c0252a345f307352a4dbc1b56e21
Tan <tvo22@uh.edu>
2019-04-17 00:17:09 -0500

    Add a bunch of fancy update feature

--- src/main/java/Main/CustomersController.java
+++ src/main/java/Main/CustomersController.java
@@ -1,6 +1,8 @@
 package Main;
 
 import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,16 +21,24 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 @Component
 public class CustomersController implements Initializable {
-    @Autowired
-    private ConfigurableApplicationContext springContext;
-    @Autowired
-    public CustomerEntityRepository customerEntityRepository;
-    public Button btnCusForm;
-    private Scene returnScene;
+    @Autowired private ConfigurableApplicationContext springContext;
+    @Autowired public CustomerEntityRepository customerEntityRepository;
+    @FXML private Button updateCustomer;
+    @FXML private Button deleteCustomer;
 
+    @FXML private TextField firstName;
+    @FXML private TextField lastName;
+    @FXML private TextField cAddress;
+    @FXML private TextField cCity;
+    @FXML private TextField cState;
+    @FXML private TextField pNum;
+    @FXML private TextField aNum;
+    @FXML private TextField cEmail;
+    @FXML private TextField cCountry;
 
     @FXML private TableView<CustomerEntity> tableView;
     @FXML private TableColumn<CustomerEntity, Float> customerNumber;
@@ -41,38 +51,44 @@ public class CustomersController implements Initializable {
     @FXML private TableColumn<CustomerEntity, String> altphone;
     @FXML private TableColumn<CustomerEntity, String> email;
 
-
-
-    @FXML private Button backtoMM;
-
-    @FXML
-    private TextField firstName;
-
-    @FXML private Button deleteCustomer;
-    @FXML
-    void backtoMM(ActionEvent event) {
-        if (returnScene == null) {
-            backtoMM.getScene().getWindow().hide();
-        } else {
-            ((Stage) backtoMM.getScene().getWindow()).setScene(returnScene);
-        }
-    }
-
+    private Scene returnScene;
     public void setReturnScene(Scene scene) {
         this.returnScene = scene;
     }
 
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        initColumns();
-        populateTable();
+    @FXML void backtoMM(ActionEvent event) {
+        var stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(returnScene);
     }
 
-
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<CustomerEntity> observableList = FXCollections.observableArrayList();
+        customerEntityRepository.findAll().forEach(observableList::add);
+        tableView.setItems(observableList);
+        tableView.getSelectionModel().selectedItemProperty().addListener((observable,oldval,newval) -> {
+            CustomerEntity cus = newval;
+            if (cus != null) {
+                firstName.setText(cus.getCustomerFname());
+                lastName.setText(cus.getCustomerLname());
+                cAddress.setText(cus.getCustomerAddress());
+                cCity.setText(cus.getCustomerCity());
+                cState.setText(cus.getCustomerState());
+                pNum.setText(cus.getCustomerPhone());
+                aNum.setText(cus.getCustomerAltcontact());
+                cEmail.setText(cus.getCustomerEmail());
+                cCountry.setText(cus.getCustomerCountry());
+                tableView.refresh();
+            }
+        });
+        tableView.getSelectionModel().clearSelection();
+        initColumns();
+        populateTable();
+        this.deleteCustomer.setDisable(true);
+        this.updateCustomer.setDisable(true);
+    }
 
     public void initColumns(){
-
         customerNumber.setCellValueFactory(new PropertyValueFactory<>("customerId"));
-
         CustomerFName.setCellValueFactory(new PropertyValueFactory<>("customerFname"));
         CustomerLName.setCellValueFactory(new PropertyValueFactory<>("customerLname"));
         address.setCellValueFactory(new PropertyValueFactory<>("customerAddress"));
@@ -81,10 +97,24 @@ public class CustomersController implements Initializable {
         phone.setCellValueFactory(new PropertyValueFactory<>("customerPhone"));
         altphone.setCellValueFactory(new PropertyValueFactory<>("customerAltcontact"));
         email.setCellValueFactory(new PropertyValueFactory<>("customerEmail"));
-
     }
 
-
+    public void addCustomer()
+    {
+        CustomerEntity cus = new CustomerEntity();
+        cus.setCustomerFname(firstName.getText());
+        cus.setCustomerLname(lastName.getText());
+        cus.setCustomerAddress(cAddress.getText());
+        cus.setCustomerCity(cCity.getText());
+        cus.setCustomerState(cState.getText());
+        cus.setCustomerPhone(pNum.getText());
+        cus.setCustomerAltcontact(aNum.getText());
+        cus.setCustomerEmail(cEmail.getText());
+        cus.setCustomerCountry(cCountry.getText());
+        tableView.getItems().add(cus);
+        customerEntityRepository.save(cus);
+        tableView.refresh();
+    }
 
 //    public void populateTable1 ()
 //    {
@@ -95,9 +125,7 @@ public class CustomersController implements Initializable {
 
     public void populateTable ()
     {
-
-
-//         productEntity = new ProductEntity();
+//        productEntity = new ProductEntity();
 //        productEntity.setProductname(productname);
 //        productEntity.setProductdesc(Description);
 //
@@ -116,25 +144,49 @@ public class CustomersController implements Initializable {
 
     }
 
-
-    public void handleCusForm(ActionEvent actionEvent)throws IOException {
-        Stage parent  = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CustomerForm.fxml"));
-        fxmlLoader.setControllerFactory(springContext::getBean);
-        Scene scene = new Scene(fxmlLoader.load());
-        CustomerFormController customerFormController = fxmlLoader.getController();
-        customerFormController.setReturnScene(btnCusForm.getScene());
-        parent.setScene(scene);
-    }
-
+//    public void handleCusForm(ActionEvent actionEvent)throws IOException {
+//        Stage parent  = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CustomerForm.fxml"));
+//        fxmlLoader.setControllerFactory(springContext::getBean);
+//        Scene scene = new Scene(fxmlLoader.load());
+//        CustomersController customerFormController = fxmlLoader.getController();
+//        customerFormController.setReturnScene(btnCusForm.getScene());
+//        parent.setScene(scene);
+//    }
 
     public void deleteCustomerClass()
     {
-        String fname = firstName.getText();
-        customerEntityRepository.deletUsersByFirstName(fname);
-        System.out.println("RECORD DELETED");
-
+//        String fname = firstName.getText();
+//        customerEntityRepository.deletUsersByFirstName(fname);
+//        System.out.println("RECORD DELETED");
+        CustomerEntity cus = tableView.getSelectionModel().getSelectedItem();
+        customerEntityRepository.delete(cus);
+        tableView.getItems().remove(cus);
+        tableView.getSelectionModel().clearSelection();
+        tableView.refresh();
+        this.deleteCustomer.setDisable(true);
+        this.updateCustomer.setDisable(true);
     }
+
+    public void updateCustomer()
+    {
+        CustomerEntity cus = tableView.getSelectionModel().getSelectedItem();
+        cus.setCustomerFname(firstName.getText());
+        cus.setCustomerLname(lastName.getText());
+        cus.setCustomerAddress(cAddress.getText());
+        cus.setCustomerCity(cCity.getText());
+        cus.setCustomerState(cState.getText());
+        cus.setCustomerPhone(pNum.getText());
+        cus.setCustomerAltcontact(aNum.getText());
+        cus.setCustomerEmail(cEmail.getText());
+        cus.setCustomerCountry(cCountry.getText());
+        customerEntityRepository.save(cus);
+        tableView.getSelectionModel().clearSelection();
+        tableView.refresh();
+        this.deleteCustomer.setDisable(true);
+        this.updateCustomer.setDisable(true);
+    }
+
 //    public void populateTable ()
 //    {
 ////        int orderID;
@@ -155,13 +207,25 @@ public class CustomersController implements Initializable {
 ////        this.componentButton.setDisable(true);
 //        populateTable();
 //    }
-//
-//    public void backButton(ActionEvent event) throws IOException
-//    {
-//        Parent tableViewParent = FXMLLoader.load(getClass().getResource("main.fxml"));
-//        Scene tableViewScene = new Scene(tableViewParent);
-//        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        window.setScene(tableViewScene);
-//        window.show();
-//    }
+
+    public void userClickedOnTable(){
+        this.deleteCustomer.setDisable(false);
+        this.updateCustomer.setDisable(false);
+    }
+
+    public void clear()
+    {
+        tableView.getSelectionModel().clearSelection();
+        firstName.setText(null);
+        lastName.setText(null);
+        cAddress.setText(null);
+        cCity.setText(null);
+        cState.setText(null);
+        pNum.setText(null);
+        aNum.setText(null);
+        cEmail.setText(null);
+        cCountry.setText(null);
+        this.deleteCustomer.setDisable(true);
+        this.updateCustomer.setDisable(true);
+    }
 }
